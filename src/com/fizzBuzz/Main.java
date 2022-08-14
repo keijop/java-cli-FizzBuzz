201a4edcd850d201108ce23db30ad6136223c536
Keijo Prunt <keijo.prunt@vincit.fi>
2022-08-14 12:15:28 +0300

    DRY - add method isDivisible

--- src/com/fizzBuzz/Main.java
+++ src/com/fizzBuzz/Main.java
@@ -3,20 +3,25 @@ package com.fizzBuzz;
 import java.util.Scanner;
 
 public class Main {
+
+    static boolean isDivisible (int inputNumber, int divider) {
+      return inputNumber % divider == 0;
+    };
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number");
         String input = scanner.next();
-        int number = Integer.parseInt(input);
+        int inputNumber = Integer.parseInt(input);
 
         String result;
 
-        if ((number % 3 == 0) && (number % 5 == 0)) {
+        if (isDivisible(inputNumber, 3) && isDivisible(inputNumber, 5)) {
             result = "FizzBuzz";
-        } else if (number % 3 == 0) {
+        } else if (isDivisible(inputNumber, 3))  {
             result = "Fizz";
-        } else if (number % 5 == 0) {
+        } else if (isDivisible(inputNumber, 5)) {
             result = "Buzz";
         }else {
            result = input;
